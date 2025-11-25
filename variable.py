33191386118a43c3a4419538d6b1226bbb8dee6b
Dominicmaina77 <mainadominic628@gmail.com>
2025-11-25 09:53:06 +0300

    variable

---
+++ variable.py
@@ -0,0 +1,39 @@
+a = 5
+b= "python"
+print(type(a))
+print(type(b))
+
+# strings can either use single or double quotes
+x = "analysis"
+b='analysis'
+print(type(x))
+print(type(b))
+
+# variables are case sensitive
+a=100
+A=200
+print(a)
+print(A)
+
+# variable names cannot start with a number
+# variable names can start with underscore character
+_myvar=100
+print(_myvar)
+
+# camel case
+myVariableName=100
+print(myVariableName)
+
+# pascal case
+MyVariableName=100
+print(MyVariableName)
+
+# snake case
+my_variable_name=100
+print(my_variable_name)
+
+# output many variables
+x,y,z='orange','banana','apple'
+print(x)
+print(y)
+print(z)
