f94c406f74bac462b4f35f096f4c76d2c235e451
Dominicmaina77 <mainadominic628@gmail.com>
2025-11-26 13:03:17 +0300

    arithmetic-comaprison-logical-assignment

---
+++ operator.py
@@ -0,0 +1,51 @@
+# arithmetic operator
+a=10
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)  # returns a float
+print(a%b)
+print(a//b)   # returns a integer always rounds down
+print(a**b)
+
+# assignment operator
+_a= 100
+print(_a)
+
+# addition assignment operator
+_a+=100
+print(_a)
+
+# subtraction assignment operator
+_a-=100
+print(_a)
+
+# multiplication assignment operator
+_a*=100
+print(_a)
+
+# comparison operator
+a=50
+b=50
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b) 
+
+
+# logical operator
+# and or not
+# and returns true if both operands are true
+# or returns true if any of the operands are true
+# not returns the opposite, if the operand is true, returns false
+a=True
+b=False
+x= 10
+print(x>5 and x<15)
+print(x>5 or x<15)
+print(a and b)
+print(a or b)
+print(not a)
