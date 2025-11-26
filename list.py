74df57b9a72454693e962b0767276cff4e608153
Dominicmaina77 <mainadominic628@gmail.com>
2025-11-26 14:20:14 +0300

    intro-list

---
+++ list.py
@@ -0,0 +1,8 @@
+# used to store multiple items in a single variable
+# list is a collection which is ordered and changeable
+# allows duplicate members
+# list is one of 4 built-in data types in python used to store collections of items
+# list is created using square brackets
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
